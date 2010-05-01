b1be85c8f9ec8fbe232f1b15f2407ea394726933
dawnofdigital <dawnofdigital@4790080a-56e1-a694-29ca-e64e70452073>
2010-05-01 02:52:26 +0000

    Added three 3rd party libraries. JGame for graphics. MINA for networking. XStream for XML.

--- trunk/ssb-riot/src/riot/Riot.java
+++ trunk/ssb-riot/src/riot/Riot.java
@@ -1,8 +1,19 @@
 package riot;
 
-public class Riot {
+import jgame.platform.JGEngine;
+
+public class Riot extends JGEngine {
+	private static final long serialVersionUID = -6417663999978098545L;
 
 	public static void main(String[] args) {
 		System.out.println("Hi thar.");
+	}
+
+	public void initCanvas() {
+		
+	}
+	
+	public void initGame() {
+		
 	}
 }
