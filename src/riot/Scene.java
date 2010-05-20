5ece35431fd202a9a0a4cc2042a54e9cc53647d0
Keegan <kjrandez@gmail.com>
2010-05-20 20:15:22 +0000

    Trying to make speed improvements.

--- src/riot/Scene.java
+++ src/riot/Scene.java
@@ -8,31 +8,16 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-
-import riot.gameobject.Character;
 import riot.gameobject.Map;
 
 
 public class Scene {
-
 	String serverName;
 	ArrayList<String> playerNames;
 	ArrayList<Sprite> worldSprites;
 	ArrayList<Sprite> overlaySprites;
 	Dimension worldSize;
 	Rectangle worldView;
-	
-	// Creation of a blank scene for testing
-	public Scene() {
-		serverName = "Local Test Server";
-		playerNames = new ArrayList<String>();
-		playerNames.add("Zapata");
-		playerNames.add("Solidpenguin");
-		worldSprites = new ArrayList<Sprite>();
-		overlaySprites = new ArrayList<Sprite>();
-		worldSize = new Dimension(640, 480);
-		worldView = new Rectangle(0, 0, 640, 480);
-	}
 	
 	// Assembly of a Scene on the Server Side
 	public Scene(String serverName, ArrayList<String> playerNames, ArrayList<GameObject> worldObjects, ArrayList<GameObject> overlayObjects) {
@@ -41,39 +26,19 @@ public class Scene {
 		
 		worldSprites = new ArrayList<Sprite>();
 		overlaySprites = new ArrayList<Sprite>();
-		
 		worldSize = new Dimension(640, 480);
-		
-		ArrayList<Character> characters = new ArrayList<Character>();
+		worldView = new Rectangle(0, 0, (int)worldSize.getWidth(), (int)worldSize.getHeight());
 		
 		for(GameObject object: worldObjects) {
 			Physics physics = object.getPhysics();
 			Sprite sprite = physics.getSprite();
 			worldSprites.add(sprite);
-			
 			if(object instanceof Map) {
 				worldSize = new Dimension(sprite.image.getWidth(), sprite.image.getHeight());
 			}
-			
-			if(object instanceof Character) {
-				characters.add((Character)object);
-			}
 		}
-		
-		for(GameObject object: overlayObjects) {
-			Physics physics = object.getPhysics();
-			overlaySprites.add(physics.getSprite());
-		}
-		
-		worldView = getBestFitView(characters, worldSize);
 	}
-	
-	private Rectangle getBestFitView(ArrayList<Character> characters, Dimension worldSize) {
-		int worldWidth = (int)worldSize.getWidth();
-		int worldHeight = (int)worldSize.getHeight();
-		return new Rectangle(0, 0, worldWidth, worldHeight);
-	}
-	
+
 	// Assembly of the Scene on the Client Side
 	public Scene(SpriteManager manager, byte[] rawData) {
 		serverName = "";
@@ -81,56 +46,33 @@ public class Scene {
 		worldSprites = new ArrayList<Sprite>();
 		overlaySprites = new ArrayList<Sprite>();
 		
-		if(rawData[0] == Riot.KeepAlive){ 
-			System.out.println("Keepalive received");
-		}
 		ByteArrayInputStream stream = new ByteArrayInputStream(rawData);
 		DataInputStream reader = new DataInputStream(stream);
 		
-		
-		
 		try {
-			while(reader.available() > 0) {
-				switch(reader.readByte()) {
-					case Riot.ServerName: {
-						serverName = "";
-						int count = reader.readShort();
-						for(int i = 0; i < count; i++)
-							serverName += reader.readChar();
-						break;
-					}
-					case Riot.PlayerNames: {
-						int count = reader.readShort();
-						for(int i = 0; i < count; i++)
-							playerNames.add(reader.readUTF());
-						break;
-					}
-					case Riot.ScreenLayout:
-						worldSize = new Dimension(reader.readShort(), reader.readShort());
-						worldView = new Rectangle(reader.readShort(), reader.readShort(), reader.readShort(), reader.readShort());
-						break;
-					case Riot.WorldSprites: {
-						int count = reader.readShort();
-						for(int i = 0; i < count; i++) {
-							Sprite sprite = new Sprite(manager, reader.readShort(), reader.readShort(), reader.readShort(), reader.readShort(), reader.readShort());
-							worldSprites.add(sprite);
-						}
-						break;
-					}
-					case Riot.OverlaySprites: {
-						int count = reader.readShort();
-						for(int i = 0; i < count; i++) {
-							Sprite sprite = new Sprite(manager, reader.readShort(), reader.readShort(), reader.readShort(), reader.readShort(), reader.readShort());
-							overlaySprites.add(sprite);
-						}
-						break;
-					}
-				}
+			// Read Server Name
+			int count = reader.readShort();
+			for(int i = 0; i < count; i++)
+				serverName += reader.readChar();
+			// Read World Information
+			worldSize = new Dimension(reader.readShort(), reader.readShort());
+			worldView = new Rectangle(reader.readShort(), reader.readShort(), reader.readShort(), reader.readShort());
+			// Read World Sprites
+			count = reader.readShort();
+			for(int i = 0; i < count; i++) {
+				Sprite sprite = new Sprite(manager, reader.readShort(), reader.readShort(), reader.readShort(), reader.readShort(), reader.readShort());
+				worldSprites.add(sprite);
+			}
+			// Read Overlay Sprites
+			count = reader.readShort();
+			for(int i = 0; i < count; i++) {
+				Sprite sprite = new Sprite(manager, reader.readShort(), reader.readShort(), reader.readShort(), reader.readShort(), reader.readShort());
+				overlaySprites.add(sprite);
 			}
 		}
 		catch(IOException ex) {
 			ex.printStackTrace();
-		}//}
+		}
 	}
 	
 	// Serialization of the Scene for Transmittance
@@ -140,29 +82,17 @@ public class Scene {
 		
 		try {
 			// Write Server Name Information
-			writer.writeByte(Riot.ServerName);
 			writer.writeShort(serverName.length());
 			for(int i = 0; i < serverName.length(); i++)
 				writer.writeChar(serverName.charAt(i));
-			
-			/*// Write Player Name Information
-			writer.writeByte(Riot.PlayerNames);
-			writer.writeShort(playerNames.size());
-			for(String playerName: playerNames) {
-				writer.writeUTF(playerName);
-			}*/
-			
 			// Write Screen Layout Information
-			writer.writeByte(Riot.ScreenLayout);
 			writer.writeShort(worldSize.width);
 			writer.writeShort(worldSize.height);
 			writer.writeShort((int)worldView.getMinX());
 			writer.writeShort((int)worldView.getMinY());
 			writer.writeShort((int)worldView.getWidth());
 			writer.writeShort((int)worldView.getHeight());
-			
 			// Write World Sprite Information
-			writer.writeByte(Riot.WorldSprites);
 			writer.writeShort(worldSprites.size());
 			for(Sprite sprite: worldSprites) {
 				writer.writeShort(sprite.index);
@@ -171,9 +101,7 @@ public class Scene {
 				writer.writeShort(sprite.y);
 				writer.writeShort(sprite.rotation);
 			}
-			
 			// Write Overlay Sprite Information
-			writer.writeByte(Riot.OverlaySprites);
 			writer.writeShort(overlaySprites.size());
 			for(Sprite sprite: overlaySprites) {
 				writer.writeShort(sprite.index);
@@ -182,7 +110,6 @@ public class Scene {
 				writer.writeShort(sprite.y);
 				writer.writeShort(sprite.rotation);
 			}
-			
 			return stream.toByteArray();
 		}
 		catch(IOException ex) {
