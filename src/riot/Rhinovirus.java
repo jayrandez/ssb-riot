fbcec8b439384513605ded929e37a84e9a568f47
rmacnak <rmacnak@4790080a-56e1-a694-29ca-e64e70452073>
2010-05-26 16:11:36 +0000

    Rhinovirus

---
+++ src/riot/Rhinovirus.java
@@ -0,0 +1,8 @@
+package riot;
+
+public class Rhinovirus extends Character {
+
+	public Rhinovirus(GameEngine engine, SpriteManager manager, SpawnPlatform platform) {
+		super(engine, manager, "rhinovirus", new Size(200,200), 20, platform);
+	}
+}
